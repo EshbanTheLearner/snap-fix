adc76051dd9cb6f53abdc8439e08b28dbae7246f
Eshban Suleman <eshban9492@gmail.com>
2023-03-25 11:58:32 +0500

    Serving image back to JS

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
-use base64::decode;
+use base64::{ encode, decode };
 use image::load_from_memory;
+use image::ImageOutputFormat::Png;
 
 #[wasm_bindgen]
-pub fn grayscale(encoded_file: &str) {
+pub fn grayscale(encoded_file: &str) -> String {
     
     log(&"Grayscale called".into()); 
     
@@ -15,5 +16,17 @@ pub fn grayscale(encoded_file: &str) {
     log(&"Image loaded".into());
 
     img = img.grayscale();
-    log(&"Grayscale efefct applied!".into());
+    log(&"Grayscale effect applied!".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"New image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!(
+        "data:image/png;base64,{}",
+        encoded_img
+    );
+
+    data_url
 }
